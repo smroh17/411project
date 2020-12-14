bbde71c87b018c48f08146a3570c7f4b49ae2035
kelsey-myton <kmyton19@gmail.com>
2020-12-14 01:54:44 -0500

    Revert "form"
    This reverts commit 4b21e29fb92a2105b109a684c739cb0e863b5d4a.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,118 +1,97 @@
-import React, { Component } from 'react'
-import { Button, Input, Footer, Card, CardBody, CardImage, CardTitle, CardText } from 'mdbreact';
+// import logo from './logo.svg';
+import './App.css';
+import './testStockPrice';
+import axios from 'axios';
+import * as mui from '@material-ui/core';
+//import Stock from "./Stock"
+//import { fetchData } from './testStockPrice';
+import {useEffect, useState} from "react";
 
-class Stock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      stocks:[]
-      //companyName: [],
-      //companySymbol: []
+const alpha = require('alphavantage')({key: 'qweqweqwe'});
+require('dotenv').config();
+
+// function App() {
+//   const [users, setUsers] = useState([]);
+
+//   useEffect(() => {
+//     axios.get("http://localhost:5000/api/tweets/tesla/2020-11-28").then((response) => {//tweet sentiment analysis
+//       console.log(response.data);
+//     });
+//     // axios.get("http://localhost:4000").then((response) => {
+//       // setUsers({users: response.data});
+//       // console.log(users);
+//       // console.log('asdzsd')
+//     // })
+
+//     axios.get('http://localhost:8000/api/stocks/tsla').then((response) => {//stock api
+//       console.log(response.data);
+//     });
+//   }, []); // empty array parameter ensures this call only runs once like componentDidMount, adding variables will call the function again if the variables ever get updated or changed.
+
+function fetchData(){
+  const API_KEY= process.env.API_KEY;
+  const keywords="tsla";
+  let API_Call= `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${keywords}&apikey=${API_KEY}`;
+  let stocksFunction= [];
+  
+  fetch(API_Call)
+  .then(
+    function(response) {
+      return response.json();
     }
-  }
-  render() {
-    return (
-    <div>
-      <h1>Stock Results</h1>
-    <div className="col-md-3" style={{ marginTop : '20px' }}>
-            <Card>
-                <CardBody>
-                    <p className=""><img src={blankImg} className={ "flag flag-"+code } alt={country.name} /></p>
-                    <CardTitle title={country.name}>{country.name.substring(0, 15)}{ country.name.length > 15 && "..."}</CardTitle>
-                </CardBody>
-            </Card>
-        </div>
-    </div>
-    )
-    }
-}
-
-
-class App extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            Search: '',
-            result: [],
-            companyName: '',
-            companySymbol: ''
-        }
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault()
-        const data = this.state
-        console.log(this.fetchData(data.Search));
-        this.setState({
-          result: this.fetchData(data.Search)
-      })
-        console.log("Final data is", this.state)
-
-    }
-
-    handleInputChange = (event) => {
-        event.preventDefault()
-       // console.log(event)
-       // console.log(event.target.name)
-       // console.log(event.target.value)
-       this.setState({
-           [event.target.name]: event.target.value
-       })
-    }
-
-    // componentDidMount(){
-    //     this.inputFullNameRef.current.focus()
-    // }
-  render () {
-      const {myFullName} = this.state
-    return (
-      <div>
-        <h1>Forms and Inputs</h1>
-        <p>Full name is: {myFullName}</p>
-        <form onSubmit={this.handleSubmit}>
-            <input type='text' name='Search'  onChange={this.handleInputChange}/>
-          <p><button>Search</button></p>
-        <Stock>
-
-        </Stock>
-        </form>
-      </div>
-    )
-  }
-  fetchData(keyword){
-    const pointerToThis= this; 
-    const API_KEY= process.env.API_KEY;
-    //const keywords="tsla";
-    let API_Call= `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${keyword}&apikey=${API_KEY}`;
-    let stocksFunction= [];
-    
-    fetch(API_Call)
-    .then(
-      function(response) {
-        return response.json();
+  )
+  .then(
+    function(data) {
+      console.log(data);
+      var len= data['bestMatches'].length;
+      
+      for (var i = 0; i < len; i++) {
+          stocksFunction.push([[data.bestMatches[i]["1. symbol"]],[data.bestMatches[i]["2. name"]]]);
+          
       }
-    )
-    // .then(
-    //   function(data) {
-    //     //console.log(data);
-    //     var len= data['bestMatches'].length;
-        
-    //     for (var i = 0; i < len; i++) {
-    //         stocksFunction.push([[data.bestMatches[i]["1. symbol"]],[data.bestMatches[i]["2. name"]]]);
-            
-    //     }
-    //     return stocksFunction;
-    //     //console.log(stocksFunction);
-    //   //   pointerToThis.setState({
-    //   //     // companyName: stocksFunction,
-    //   //     // companySymbol: stocksFunction
-    //   //     result: stocksFunction
-
-    //   // });
-    //   }
-    // )
+      console.log(stocksFunction);
+  
+      // // console.log(stockChartXValuesFunction);
+      // pointerToThis.setState({
+      //   stocks: stocksFunction
+      //   //st: stocksFunction;
+      // });
     }
+  )
+  }
 
-}
 
-export default App
+  function App(){
+  return (
+    <div className="App">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      <header className="App-header">
+        <p>
+          STOCK MACHINE
+        </p>
+        {/* <Stock></Stock> */}
+        <form >
+          <input 
+          id="frm1" 
+          type="text"
+          // value={search}
+          // onChange={event => setSearch(event.target.value)}/>
+          />
+        <mui.Button variant="contained" color="primary" type="submit" onClick={fetchData()}>Search</mui.Button>
+        </form>
+      {/* <p>{stockData}</p> */}
+      </header>
+    </div>
+    
+  );
+  }
+
+// Object.size = function(obj) {
+//   let size = 0, key;
+//   for (key in obj) {
+//       if (obj.hasOwnProperty(key)) size++;
+//   }
+//   return size;
+// };
+
+export default App;
